4a5115e7a7f8d30061710934af66c31024b604be
Mikael Escolin <mikaelescolin@gmail.com>
2025-01-29 20:26:37 +0000

    Update CORS for network configs and run server on network

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 
 	"knights-vow/internal/database"
 	"knights-vow/internal/middleware"
-	"knights-vow/internal/resources/uploads"
+	"knights-vow/internal/resources/files"
 	"knights-vow/internal/resources/users"
 )
 
@@ -21,19 +21,20 @@ func main() {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://192.168.68.100:5173", "http://192.168.68.103:5173"}, // Specify exact origin
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
-		MaxAge:           2 * time.Hour,
+		MaxAge:           1 * time.Hour,
 	}))
+
 	r.Use(middleware.AuthenticateUser)
 
 	v1 := r.Group("api/v1")
 
 	users.CreateRouterGroup(v1)
-	uploads.CreateRouterGroup(v1)
+	files.CreateRouterGroup(v1)
 
-	r.Run()
+	r.Run("0.0.0.0:8080")
 }
