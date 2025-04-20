d9c1c591cf59c9b93fe3ff33b31c2600bd12bfc7
Mikael Escolin <mikaelescolin@gmail.com>
2025-04-20 15:54:47 +0100

    Update main.go to use environment variables instead

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -11,6 +15,7 @@ import (
 	"knights-vow/internal/middleware"
 	"knights-vow/internal/resources/files"
 	"knights-vow/internal/resources/users"
+	"knights-vow/pkg/env"
 )
 
 func main() {
@@ -20,8 +25,18 @@ func main() {
 
 	r := gin.Default()
 
+	if err := env.LoadEnv("./.env"); err != nil {
+		log.Fatalf("Failed to load env\n%s", err)
+	}
+
+	allowedOriginsValue := os.Getenv("ALLOWED_ORIGINS")
+	if allowedOriginsValue == "" {
+		log.Fatalln("ALLOWED_ORIGINS not set")
+	}
+	allowedOrigins := strings.Split(allowedOriginsValue, ",")
+
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173", "http://192.168.68.100:5173", "http://192.168.68.103:5173"}, // Specify exact origin
+		AllowOrigins:     allowedOrigins,
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
@@ -36,5 +51,12 @@ func main() {
 	users.CreateRouterGroup(v1)
 	files.CreateRouterGroup(v1)
 
-	r.Run("0.0.0.0:8080")
+	URL := os.Getenv("URL")
+	port := os.Getenv("PORT")
+	if URL == "" || port == "" {
+		log.Fatalln("URL or PORT not set")
+	}
+
+	r.Run(fmt.Sprintf("%s:%s", URL, port))
+	// r.Run("0.0.0.0:8080")
 }
