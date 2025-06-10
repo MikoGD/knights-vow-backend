1140b29a917a7f29ef981cda64107978ee769896
Mikael Escolin <mikaelescolin@gmail.com>
2025-06-10 07:19:41 +0100

    Add users' handlers and routes

--- internal/resources/users/routes.go
+++ internal/resources/users/routes.go
@@ -1,15 +1,25 @@
 package users
 
 import (
+	"database/sql"
+
 	"github.com/gin-gonic/gin"
 )
 
-func CreateRouterGroup(r *gin.RouterGroup) {
+func CreateRouterGroup(r *gin.RouterGroup, db *sql.DB) {
 	users := r.Group("/users")
+	repository := CreateNewUserRepository(db)
+	userService := createNewUserService(repository)
 
 	{
-		users.POST("/sign-up", handleCreateUser)
-		users.POST("/login", HandleUserLogin)
-		users.GET("/:id/auth-status", CheckUserAuthStatus)
+		users.POST("/sign-up", func(c *gin.Context) {
+			handleCreateUser(c, userService)
+		})
+		users.POST("/login", func(c *gin.Context) {
+			HandleUserLogin(c, userService)
+		})
+		users.GET("/:id/auth-status", func(c *gin.Context) {
+			CheckUserAuthStatus(c, userService)
+		})
 	}
 }
